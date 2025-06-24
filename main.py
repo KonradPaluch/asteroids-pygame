5fb8cd1ed4c74d029633a4f411c34d48912df8d3
Konrad Paluch <konradpaluch97@gmail.com>
2025-06-24 19:20:40 +0300

    initial game loop

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, \
+    ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, \
+    ASTEROID_SPAWN_RATE
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(
+        [SCREEN_WIDTH, SCREEN_HEIGHT],
+        pygame.SCALED)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(("black"))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
